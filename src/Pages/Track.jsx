f6d313edc7b5d8448d3ab2a653ace7a9b6683e33
Sameshnee21 <2490510@students.wits.ac.za>
2024-05-23 23:24:48 +0200

    map

--- src/Pages/Track.jsx
+++ src/Pages/Track.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import "../style/home.css";
+import "../style/map.css";
+import map from "../Images/IsleofMan Map.png";
 
 function Track() {
     return (
       <div>
         <h1 className="Title">Routes of the TT</h1>
+<img className="map" src={map} alt="Isle of Man Map and track" />
+
         </div>
 
         
