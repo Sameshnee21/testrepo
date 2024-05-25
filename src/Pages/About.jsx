5ca977d335d3b2e7eefa0adfc895db5418df97df
Sameshnee21 <2490510@students.wits.ac.za>
2024-05-25 19:59:03 +0200

    about and track

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import "../style/home.css";
 import "../style/about.css";
 import profile from "../Images/Portrait.png";
+import logo1 from "../Images/Logo 1.png";
+
 
 
 
@@ -10,9 +12,13 @@ function About() {
       <div>
         <h1 className="Title">Meet The Designer: Sameshnee Moodley</h1>
         <img className="pic" src={profile} alt="Biker with helmet with a heart sign." />
-        <p className="Profile">Hey there! I'm Sameshnee, a graphic designer, animator and a bike enthusiast.  </p>
+        <p className="Profile">Hey there! I'm Sameshnee, a graphic designer, animator and a bike enthusiast. I hope that you have enjoyed this webiste as much as I have had, building it. I built my website based on the bond that motorbikes have created between my brother and I. Not many know how special the motorbike world is, it is not merely a sport but rather something magical (and addictive).</p>
         <p className="Profile"> As the saying goes, "Input makes the design better" (well, something like that), all input is much appreciated so that we can improve and make your experience wayyyyyy better.</p>
-        <p className="Profile">If you would like to see more of my work, you can follow me on Instagram, where I post the good, the bad and the ugly and Behance (which is mostly the good).</p>
+        <p className="Profile">If you would like to see more of my work, you can follow me on Instagram (@the_sameshnee), where I post the good, the bad and the ugly, Behance (which is mostly the good) and LinkedIn.</p>
+        <p className="Profile">Much Love, Sameshnee.</p>
+
+<img className="logo" src={logo1} alt="instagram and behance logo" />
+
        {/*Insert Social media Handles or links*/ }
        
         </div>
--- src/Pages/Track.jsx
+++ src/Pages/Track.jsx
@@ -2,12 +2,27 @@ import React from "react";
 import "../style/home.css";
 import "../style/map.css";
 import map from "../Images/IsleofMan Map.png";
+import sector4 from "../Images/Sector4.png";
+import sector6 from "../Images/Sector6.png";
+import sector8 from "../Images/Sector8.png";
+import sector9 from "../Images/Sector9.png";
+import sector11 from "../Images/Sector11.png";
+import sector12 from "../Images/Sector12.png";
 
 function Track() {
     return (
       <div>
         <h1 className="Title">Routes of the TT</h1>
 <img className="map" src={map} alt="Isle of Man Map and track" />
+
+<h2 className="Title">Here are a few fan favourite locations to look out for!</h2>
+
+<img className="left" src={sector4} alt="racetrack with a cottage" />
+<h3 className="Sub1"> SECTOR 4: SARAH'S COTTAGE</h3>
+<p className="text1"> cheeseball</p>
+
+
+{/*<p className="P">Images courtesy of TT Marshalls.</p>*/}
 
         </div>
 
