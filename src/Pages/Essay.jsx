f9bfb21d8adb8dacb12713461f651dd221fecb01
Sameshnee21 <2490510@students.wits.ac.za>
2024-05-26 16:01:11 +0200

    essay additions

--- src/Pages/Essay.jsx
+++ src/Pages/Essay.jsx
@@ -1,6 +1,9 @@
 import React from "react";
-
 import "../style/home.css";
+import "../style/essay.css";
+import essay1 from "../Images/essay1.png";
+import essay2 from "../Images/essay2.png";
+
 
 function Essay() {
     return (
@@ -71,6 +74,159 @@ an industrialized and technologically advanced world where no borders exist.
 </p>
 
 <h3 className="Title">INDIRECT FLIGHTS BY JOE HAMILTON (2014) </h3>
+
+<img className="pic" src={essay1} alt="" />
+<img className="pic" src={essay2} alt="" />
+
+<h3 className="Title">ABOUT THE ARTWORK</h3>
+<p className="P">“Indirect Flights” was designed by Joe Hamilton in 2014 consisting of photography that
+he shot while traveling to Europe, Asia and Middle East (Google Arts and Culture, n.d).
+The artwork is an online piece that is made up of print, sculpture and video forms to
+create a “layered digital topography” (Google Arts and Culture, n.d), that mimics the
+Google maps layout. The zoom-in and zoom-out feature constructs “micro-domains”
+(Dekker, 2018) for the user to explore. This feature mimics the Google Maps aesthetics
+of interacting with the environment by moving around the artwork. The artwork seems
+to loop continuously like a never ending scene which makes user want to experience
+the entire artwork.</p>
+
+<p className="P">A range of “raw materials, satellite images, organic textures, brush strokes and
+architectural fragments” were included to create the artwork, in other words, mixed
+media. This “non-linear approach” (Dekker, 2018) creates depth and a unique
+interpretation as compared to traditional topography with a start and end. Again the
+“non-linear approach” provides a sense that a user can experience more but at the
+same time appreciate every dimension.</p>
+
+<p className="P">The spacial design, colours and mixture of sound devices gives an impression of depth
+and navigation as the user moves around the artwork. The layering of imagery and the
+colours of each element creates a sense of authenticity of the places that the artist
+travelled to. It is an important element in the design as it contributes towards the mood
+and emotional state of the user at that point in time. If the design lacked colour, the
+mood of the artwork would create a melancholic environment. The use of natural or
+rather realistic colours leaves room for the user to interpret the artwork freely.</p>
+
+<p className="P">The sound devices used in the artwork, is another important element that creates a
+sense of realism, this technique is also known as a “ hyperreal ” effect (Google Arts and
+Culture, n.d). It is made up of footsteps, creaks, electrical static and wind. Sound
+devices are known to amplify the user experience and integrate reality with the
+experience.</p>
+
+<h3 className="Title">CHALLENGES/ INNOVATION AND TRANSFORMATION COMPARED TO
+TRADITIONAL ART.</h3>
+
+<p className="P">Net art is categorized as a “technical media of display” (Bruhn and Schirrmacher,
+2021) as the artwork is distributed by mass media, which is the internet. This means
+that a larger audience can engage with the artwork as compared to an audience in an
+art gallery. This approach encourages more interaction and communication amongst
+the digital community. Arguably, the digital community is an ever expanding community
+with a strong appreciation for dynamic interactive media.</p>
+
+<p className="P">The main challenge evident in this artwork is familiarity, culture and social norms. What
+if the user is not familiar with Europe, Asia or the Middle East? Will the user experience
+be compromised or is that what the artist intended? The fragments of information,
+being the overlapping photography, “creates documentation of the past” (Dekker,
+2018) which is a crucial method to interact with the target audience or user.</p>
+
+<p className="P">Users who have interacted with or are familiar with the places in the artwork will have
+a stronger emotional attachment than those who aren’t. They may not appreciate the
+artwork as much and therefore have no emotional contact with the artist’s intention.
+This is known as “cognitive import” and is defined by a series of signs or symbols that
+“produce a mental significance” in the user or audience (Ellestrom, 2021). The use of
+cognitive import creates patterns within the artwork for the user to appreciate. These
+patterns are the contact point where artist meets user in a mutual space.</p>
+
+<p className="P">The innovation and transformation compared to tradition art is evident in many aspects
+such as the use of “digital textures” that are used to create a collage of illusion. The
+brushstrokes and variations in photography imitate a traditional collage, whilst the
+sound effects add an innovative twist to the artwork. This contributes to the artworks
+uniqueness and helps it standout from traditional art collages thus adding a sense of
+reality.</p>
+
+<h3 className="Title">HOW DOES IT CREATE MEANING/ EMOTION AND ENGAGE THE AUDIENCE?</h3>
+
+<p className="P">The photos of artifacts, textures and settings, create an emotional attachment to a
+place. Each photo layered upon the map can be recognized by an audience specific
+to that area. Artifacts, places and even brush strokes (evident in the artwork) tend to
+carry history which contributes to the storytelling in the artwork. By using what is known
+or familiar to people from that specific area, the artwork compels the audience or user
+to network with others that feel the same way. This allows them to connect on a mutual
+platform such as the internet. The internet’s purpose, as stated by Gibson (2004), is
+to connect users with digital environments, which is evident in Hamilton’s “Indirect
+Flights” (2014).</p>
+
+<p className="P">Ellestrom (2021) describes cognitive imports of media as an “extension of the mind”
+or rather a transfer of information, which means that the user’s body becomes a media
+product. By becoming a media product to an artwork, an emotional attachment is
+created between artwork and audience or user. Each translation or experience of the
+artwork will differ depending on the audience and their knowledge of current affairs
+and experiences which is a dimension intermingled with interpretation.
+</p>
+
+<h3 className="Title">CONCLUSION</h3>
+
+<p className="P">In conclusion, Net art is a complex yet powerful medium of expression. One should
+however keep in mind that it cannot replace the uniqueness of traditional art, nor can
+the norms and standards of traditional art be replaced. Each artform or medium has
+its own characteristics, one cannot replace the other, but it can be used to collaborate
+with, in order to create an “immersive environment” for people (Gibson, 2004).</p>
+
+<p className="P">Ricci (2020) makes a clear statement that although Net art has opened doors to new
+perspectives, it has at the same time ushered new concerns, which challenge the
+existing “authorship and authenticity” of the work. The concern comes from technology
+constantly being updated which makes older Net art outdated. The complexity of these
+challenges will exist as future digital technologies become obsolete or advanced. Net
+art, however, is the future and the future is change. With change imminent, art has to
+be agile, adaptable and appeal to evolving tastes of the user.
+</p>
+
+
+<div className="sources">
+<h3 className="SC">SOURCES CONSULTED</h3>
+<p className="SCT">- Bruhn, J. and Schirrmacher,B.(2021). Media combination, transmediation and
+media representation. Intermedial Studies, pp.103–105.doi:https://doi.org/10.4324/9781003174288-7.</p>
+
+<p className="SCT">- Chatel, M. (2019). Net Art, Post-internet Art, New Aesthetics The Fundamentals of Art
+on the Internet. [online] Medium. Available at: https://medium.com/danae/net-art-post-
+internet-art-new-aesthetics-the-fundamentals-of-art-on-the-internet-55dcbd9d6a5
+Accessed: 25 April 2024.</p>
+
+<p className="SCT">- Carrier, D. (2020). The Work of Art in the Age of the Internet. [online]
+Hyperallergic. Available at: https://hyperallergic.com/563938/the-work-of-art-in-
+the-age-of-the-internet/
+Accessed: 25 April 2024</p>
+
+<p className="SCT">- Dekker, A. (2018). Collecting and Conserving Net Art. Routledge.</p>
+
+<p className="SCT">- Elleström, L. (2021). Beyond media borders : intermedial relations among
+multimodal media. Cham, Switzerland: Palgrave Macmillan.</p>
+
+<p className="SCT">- Gibson, S. (2004). Perspectives on Multimedia: Communication, Media and
+Inforamtion Technology. London: John Wiley and Sons.</p>
+
+<p className="SCT">- Google Arts & Culture. (n.d.). Indirect Flights - Joe Hamilton. [online] Available at:
+https://artsandculture.google.com/asset/indirect-flights-joe-hamilton/mAGK9-
+LDjcjHVg?hl=en Accessed: 25 April 2024.</p>
+
+<p className="SCT">- Indirect Flights. (2014). Indirect Flights. [online] Available at: http://indirect.flights
+Accessed: 25 April 2024.</p>
+
+<p className="SCT">- Los Angeles Review of Books. (2023). What Is Internet Art Actually Doing? On
+Omar Kholeif’s ‘Internet_Art’. [online] Available at:
+https://lareviewofbooks.org/article/what-is-internet-art-actually-doing-on-omar-
+kholeifs-internet_art/
+Accessed: 25 April 2024.</p>
+
+<p className="SCT">- Mailloux, S. (1983). Convention and Context. New Literary History, 14(2), p.399.
+doi:https://doi.org/10.2307/468693.</p>
+
+<p className="SCT">- Ricci, B. (2020). Net Art and How The Internet Has Created A New Medium. [online]
+Artland Magazine. Available at: https://magazine.artland.com/agents-of-change-
+internet-net-art-how-the-world-wide-web-has-affected-the-way-we-make-art/
+Accessed: 25 April 2024.</p>
+
+<p className="SCT">- Tunali, T. (2016). Net Art and Activism. Studies in Visual Arts and Communication: an
+international journal, 3(1).
+</p>
+</div>
 
         </div>
     );
